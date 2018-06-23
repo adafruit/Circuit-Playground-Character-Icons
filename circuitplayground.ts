94edd5cb8ec45f2d0d40a99e17e343ba0828bbb8
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-06-22 17:57:14 -0700

    fixing images

---
+++ circuitplayground.ts
@@ -0,0 +1,14 @@
+namespace circuitplayground {
+    //% fixedInstance jres blockIdentity=image.__imagePicker
+    export const charactersAdabot = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=image.__imagePicker
+    export const charactersBlinka = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=image.__imagePicker
+    export const charactersMho = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=image.__imagePicker
+    export const charactersMinerva = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=image.__imagePicker
+    export const charactersSparky = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=image.__imagePicker
+    export const characters5 = image.ofBuffer(hex``);
+}
