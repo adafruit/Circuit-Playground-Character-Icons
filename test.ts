693ac4c22c1ba2747175d1c6cd6a3eeca8e3bdfa
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-04-03 14:04:20 -0700

    added test program

--- test.ts
+++ test.ts
@@ -1,1 +1,13 @@
 // tests go here; this will not be compiled when this package is used as a library
+[
+    circuitplayground.Adabot,
+    circuitplayground.Blinka,
+    circuitplayground.Mho,
+    circuitplayground.Minerva,
+    circuitplayground.Sparky
+].forEach(i => {
+    let sprite = sprites.create(i)
+    sprite.setVelocity(Math.randomRange(-60, 60), Math.randomRange(-60, 60))
+    sprite.setFlag(SpriteFlag.BounceOnWall, true)
+})
+effects.starField.startScreenEffect()
